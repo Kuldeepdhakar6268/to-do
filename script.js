2560a0bb0799f3cb776eafeda481ff7dc036432f
Kuldeepdhakar6268 <dhakarkuldeep6268@gmail.com>
2024-02-27 08:42:26 +0530

    done button

--- script.js
+++ script.js
@@ -1,39 +1,41 @@
-let input=document.querySelector(".in");
-let addbutton=document.querySelector(".addbutton");
+let input=document.querySelector(".input");
+let addbutton=document.querySelector(".add");
 let ul=document.querySelector("ul");
+
 addbutton.addEventListener("click",function(){
-    if(input.value!=""){
-    let task=input.value;
-    input.value="";
-
-    let li=document.createElement("li");
-
-    li.innerHTML=task+`<button class="del">Delete</button><button class="edit">Edit</button>`;
-
-//this functionality is delete button
-
+  if(input.value!=""){
+  let task=input.value;
+  input.value="";
+  let li=document.createElement("li");
+  li.innerHTML=task+`<button class="del">Delete</button><button class="edit">Edit</button><button class="done">Done</button>`
   let del=li.querySelector(".del");
+  let edit=li.querySelector(".edit");
+  
+  // edit button functionality
+  let flag=true;
+  edit.addEventListener("click",function(){
+    if(flag==true){
+    input.value=li.childNodes[0].textContent;
+    flag=false;
+    }
+    else{
+      li.childNodes[0].textContent=input.value;
+      input.value="";
+      flag=true;
+    }
+  })
+
+  //delete button functionality
   del.addEventListener("click",function(){
     li.remove();
   })
 
-  // this functionnality is edit button
- let flag=true;
-  let edit=li.querySelector(".edit");
+  let done=li.querySelector(".done");
   
-  edit.addEventListener("click",function(){
-  if(flag==true){
-  input.value=li.childNodes[0].textContent;
-  flag=false;
-  }
-  else{
-    li.childNodes[0].textContent=input.value;
-    input.value="";
-    flag=true;
-  }
+  done.addEventListener("click",function(){
+  li.classList.toggle("textdelete")
     
   })
-
-    ul.appendChild(li);
+  ul.appendChild(li);
 }
 })
