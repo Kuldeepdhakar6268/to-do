c30f10fa82e29a5f1c3b29fa98ce444ffc7b0ab9
Kuldeepdhakar6268 <dhakarkuldeep6268@gmail.com>
2024-02-28 07:43:35 +0530

    localstorage

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 let input=document.querySelector(".input");
 let addbutton=document.querySelector(".add");
 let ul=document.querySelector("ul");
+let count=1;
 
 addbutton.addEventListener("click",function(){
   if(input.value!=""){
   let task=input.value;
   input.value="";
   let li=document.createElement("li");
-  li.innerHTML=task+`<button class="del">Delete</button><button class="edit">Edit</button><button class="done">Done</button>`
+  li.innerHTML=`<span class=inputtext>${task}</span><button class="del">Delete</button><button class="edit">Edit</button>   <button class="done">Done</button>`
   let del=li.querySelector(".del");
   let edit=li.querySelector(".edit");
   
@@ -31,11 +32,16 @@ addbutton.addEventListener("click",function(){
   })
 
   let done=li.querySelector(".done");
-  
+  let inputtext=li.querySelector(".inputtext")
   done.addEventListener("click",function(){
-  li.classList.toggle("textdelete")
-    
+   inputtext.classList.toggle("textdelete");
   })
+  localStorage.setItem(count,JSON.stringify(li.childNodes[0].textContent));
+  count++;
+  console.log(localStorage.getItem(5))
+  
   ul.appendChild(li);
+  
 }
 })
+
